a4a976d9baa62be247e4fa67258ae46148326eef
Miguel Martins <miguelrjmartins9@gmail.com>
2023-01-26 14:40:17 -0400

    settings.py update

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zv_@fvak3kt&v@wp__$d+jemjb3h-capv&)xjf4++p(qhe)4%4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.207.213', 'localhost', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '0.0.0.0']
 
 
 # Application definition
